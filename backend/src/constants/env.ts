898d4622ad7eb1eb3fcfbba73dd104a37ce9b50e
Nikola Kenjic <nikola.nico89@gmail.com>
2025-03-06 23:13:15 +0100

    send mail

--- backend/src/constants/env.ts
+++ backend/src/constants/env.ts
@@ -16,4 +16,4 @@ export const APP_ORIGIN = getEnv('APP_ORIGIN');
 export const JWT_SECRET = getEnv('JWT_SECRET');
 export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
 // export const EMAIL_SENDER = getEnv('EMAIL_SENDER');
-// export const RESEND_API_KEY = getEnv('RESEND_API_KEY');
+export const RESEND_API_KEY = getEnv('RESEND_API_KEY');
