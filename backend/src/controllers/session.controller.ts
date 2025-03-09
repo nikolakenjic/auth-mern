2728a4a159494637f93560be5b0f27616ffd4708
Nikola Kenjic <nikola.nico89@gmail.com>
2025-03-09 17:13:28 +0100

    delete session handler

--- backend/src/controllers/session.controller.ts
+++ backend/src/controllers/session.controller.ts
@@ -1,10 +1,15 @@
-import { OK } from '../constants/http';
+import { z } from 'zod';
+import { NOT_FOUND, OK } from '../constants/http';
 import SessionModel from '../models/session.model';
 import catchAsync from '../utils/catchAsync';
+import appAssert from '../utils/appAssert';
 
 export const getSessionsHandler = catchAsync(async (req, res, next) => {
   const sessions = await SessionModel.find(
-    { userId: req.userId, expiresAt: { $gt: new Date() } },
+    {
+      userId: req.userId,
+      expiresAt: { $gt: Date.now() },
+    },
     {
       _id: 1,
       userAgent: 1,
@@ -16,9 +21,24 @@ export const getSessionsHandler = catchAsync(async (req, res, next) => {
   );
 
   return res.status(OK).json(
+    // mark the current session
     sessions.map((session) => ({
       ...session.toObject(),
-      ...(session.id === req.sessionId && { current: true }),
+      ...(session.id === req.sessionId && {
+        isCurrent: true,
+      }),
     }))
   );
+});
+
+export const deleteSessionHandler = catchAsync(async (req, res, next) => {
+  const sessionId = z.string().parse(req.params.id);
+
+  const deleted = await SessionModel.findOneAndDelete({
+    _id: sessionId,
+    userId: req.userId,
+  });
+  appAssert(deleted, NOT_FOUND, 'Session not found');
+
+  return res.status(OK).json({ message: 'Session removed' });
 });
