6bd5026da20841813c1a68ebd701718ec3bdfd81
Nikola Kenjic <nikola.nico89@gmail.com>
2025-03-12 22:40:38 +0100

    add shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,15 @@
+import { Button } from './components/ui/button';
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="ml-10">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-xl font-bold underline text-red-400">
+        Hello from React
+      </p>
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default App;
