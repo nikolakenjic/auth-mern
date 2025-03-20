415bf6f0e56fce721dad9aeae225b2c04d6625fe
Nikola Kenjic <nikola.nico89@gmail.com>
2025-03-20 19:40:41 +0100

    add reset password api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -9,6 +9,11 @@ export interface registerParams extends loginParams {
   confirmPassword: string;
 }
 
+export interface resetPasswordParams {
+  password: string;
+  verificationCode: string;
+}
+
 export const login = async (data: loginParams) => API.post('/auth/login', data);
 export const register = async (data: registerParams) =>
   API.post('/auth/register', data);
@@ -16,3 +21,8 @@ export const verifyEmail = async (code: string) =>
   API.get(`/auth/email/verify/${code}`);
 export const sendPasswordResetEmail = async (email: string) =>
   API.post('/auth/password/forgot', { email });
+export const resetPassword = async ({
+  verificationCode,
+  password,
+}: resetPasswordParams) =>
+  API.post('/auth/password/reset', { verificationCode, password });
